cadc82bc792de5639f683fa0721ec118900e874e
Kyle Kozma <kylekozma@gmail.com>
2014-12-23 14:30:11 -0800

    tests for signup page

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
 	end
 	assert_template 'users/new'
+	assert_select 'div#error_explanation'
+	assert_select 'div.field_with_errors'
 	end
 
 	test "valid signup information" do
@@ -25,5 +27,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
 		end
 		assert_template 'users/show'
+		assert_not flash = nil
 	end
 end
