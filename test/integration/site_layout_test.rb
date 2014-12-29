9504ce96c20679cdd9b6c3297045917cf8b8404f
Kyle Kozma <kylekozma@gmail.com>
2014-12-29 12:52:42 -0800

    created new partial for edit/new pages'

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+	def setup
+		@user = users(:kyle)
+		@other_user = users(:archer)
+	end
+
+
 	test "layout links" do
 		get root_path
 		assert_template 'static_pages/home'
@@ -11,5 +17,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", contact_path	
 		get signup_path
 		assert_select "title", full_title("Sign up")	
+	end
+
+	test "layout links when logged in" do
+		log_in_as(@user)
+		get root_path
+		assert_template 'static_pages/home'
+		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", help_path
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+		get signup_path
+		assert_select "title", full_title("Sign up")
 	end
 end
