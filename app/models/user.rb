2cd2b0cb9d16dda6b72399615a99c3af3cc3b468
Kyle Kozma <kylekozma@gmail.com>
2014-12-30 01:28:15 -0800

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save { email.downcase! }
 	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
@@ -51,6 +51,22 @@ class User < ActiveRecord::Base
   	UserMailer.account_activation(self).deliver_now
   end
 
+# Sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 private
 
 # Converts email to all lower-case.
@@ -58,6 +74,7 @@ private
   	self.email = email.downcase
   end
 
+ # Creates and assigns the activation token and digest. 
   def create_activation_digest
   	self.activation_token = User.new_token
   	self.activation_digest = User.digest(activation_token)
