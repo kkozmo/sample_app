c5aa630c40bf77cb2c5a1ea31e4f25fcd73f4687
Kyle Kozma <kylekozma@gmail.com>
2015-01-01 10:01:55 -0800

    Fixes to migration

--- db/migrate/20141231063907_create_microposts.rb
+++ db/migrate/20141231063907_create_microposts.rb
@@ -5,6 +5,6 @@ class CreateMicroposts < ActiveRecord::Migration
       t.references :user, index: true
       t.timestamps null: false
     end
-    add_foreign_key :microposts, [:user_id, :created_at]
+    add_index :microposts, [:user_id, :created_at]
   end
 end
